1aadbd1994df21c6a1281a1c27733c0fe1897a79
Will LE <lexuandinhct@gmail.com>
2023-09-25 10:30:32 +0700

    reset from 3-column layout
    Signed-off-by: Will LE <lexuandinhct@gmail.com>

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+//import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-const d3main = d3.selectAll("#mainSVG");
+//const d3main = d3.selectAll("#mainSVG");
 
-d3main.selectAll("circle")
-  .data(d3.range(100))
-  .join("circle")
-    .attr("cx", -300+50)
-    .attr("cy", d => d * 50 - 300)
-    .attr("r", d => Math.abs(Math.cos(d)) * 20)
-    .attr("fill", "blue")
-    .on("mouseover", (e,d)=>{
-      d3.select(e.target).style("fill", "orange");
-      //console.log(`[${d}] xy(${e.offsetX},${e.offsetY})`,e.target,'from',e.fromElement,  e);
-    })
-    .on("mouseout", (e)=>{
-      d3.select(e.target).style("fill", "");
-      //console.log(`out xy(${e.offsetX},${e.offsetY})`,e.target,'to',e.toElement,  e);
-    })
+function scrollRightAll(el){
+    // "justify-content: safe right" just falls back to "start", i.e. left alignment.
+    // so here we must manually scroll all to the right
+    //console.log(`Before: scrollLeft = ${el.scrollLeft}; scrollWidth = ${el.scrollWidth}; clientWidth = ${el.clientWidth}`);
+    el.scrollLeft = el.scrollWidth - el.clientWidth;
+    //console.log(`After: scrollLeft = ${el.scrollLeft}; scrollWidth = ${el.scrollWidth}; clientWidth = ${el.clientWidth}`);
+}
+window.scrollRightAll = scrollRightAll;
 
+const breadcrumbs = document.getElementById("breadcrumbs");
+breadcrumbs.addEventListener("wheel", function (e) {
+    // Map the vertical scroll (e.deltaY) to the horizontal scroll (e.deltaX with Shift-Scroll)
+    if (e.deltaY != 0) this.scrollLeft += e.deltaY * 0.2;
+});
+
+const indexViewCtrl = document.getElementById("indexViewCtrl");
+const resizeObsvr = new ResizeObserver(function (es) {
+    for(const e of es) {
+        //console.log(`${e.target.id} resized to ${e.contentBoxSize[0].inlineSize}`);
+    }
+    scrollRightAll(breadcrumbs);
+});
+resizeObsvr.observe(indexViewCtrl);
 
